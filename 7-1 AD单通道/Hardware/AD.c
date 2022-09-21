43df79782dc0ba4602d89d2af5d279d3c70dba70
2104117713@qq.com <2104117713@qq.com>
2022-09-21 19:54:46 +0800

    第三次更新

--- 7-1 AD单通道/Hardware/AD.c
+++ 7-1 AD单通道/Hardware/AD.c
@@ -1,10 +1,12 @@
 #include "stm32f10x.h"                  // Device header
 
+uint16_t AD_Value[4];
+
 void AD_Init(void)
 {
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1,ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
-	
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
 	RCC_ADCCLKConfig(RCC_PCLK2_Div6);
 	
 	GPIO_InitTypeDef GPIO_InitStructure;
@@ -13,16 +15,37 @@ void AD_Init(void)
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA,&GPIO_InitStructure);
 	
+	ADC_RegularChannelConfig(ADC1,ADC_Channel_0,1,ADC_SampleTime_55Cycles5);	
+	ADC_RegularChannelConfig(ADC1,ADC_Channel_1,2,ADC_SampleTime_55Cycles5);	
+	ADC_RegularChannelConfig(ADC1,ADC_Channel_2,3,ADC_SampleTime_55Cycles5);	
+	ADC_RegularChannelConfig(ADC1,ADC_Channel_3,4,ADC_SampleTime_55Cycles5);	
+	
 	ADC_InitTypeDef ADC_InitStructure;
 	ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
 	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
 	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
 	ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
-	ADC_InitStructure.ADC_ScanConvMode = DISABLE;
-	ADC_InitStructure.ADC_NbrOfChannel = 1;
+	ADC_InitStructure.ADC_ScanConvMode = ENABLE;
+	ADC_InitStructure.ADC_NbrOfChannel = 4;
 	ADC_Init(ADC1,&ADC_InitStructure);
 	
+	DMA_InitTypeDef DMA_InitStructure;
+	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&ADC1->DR;
+	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+	DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)AD_Value;
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	DMA_InitStructure.DMA_BufferSize = 4;
+	DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+	DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+	DMA_InitStructure.DMA_Priority = DMA_Priority_Medium;
+	DMA_Init(DMA1_Channel1,&DMA_InitStructure);
+	
+	DMA_Cmd(DMA1_Channel1,ENABLE);
 	ADC_Cmd(ADC1,ENABLE);
+	ADC_DMACmd(ADC1,ENABLE);
 	
 	ADC_ResetCalibration(ADC1);							//手册建议配置完进行校准
 	while(ADC_GetResetCalibrationStatus(ADC1) == SET);  //复位校准
@@ -30,10 +53,14 @@ void AD_Init(void)
 	while(ADC_GetCalibrationStatus(ADC1) == SET);		//启动校准
 }
 
-uint16_t AD_GetValue(uint8_t ADC_Channel)
+void AD_GetValue(void)
 {
-	ADC_RegularChannelConfig(ADC1,ADC_Channel,1,ADC_SampleTime_55Cycles5);	
+	DMA_Cmd(DMA1_Channel1,DISABLE);
+	DMA_SetCurrDataCounter(DMA1_Channel1,4);
+	DMA_Cmd(DMA1_Channel1,ENABLE);
+	
 	ADC_SoftwareStartConvCmd(ADC1,ENABLE);
-	while(ADC_GetFlagStatus(ADC1,ADC_FLAG_EOC) == RESET);
-	return ADC_GetConversionValue(ADC1);
+	
+	while(DMA_GetFlagStatus(DMA1_FLAG_TC1) == RESET);
+	DMA_ClearFlag(DMA1_FLAG_TC1);
 }
--- 7-1 AD单通道/Hardware/AD.h
+++ 7-1 AD单通道/Hardware/AD.h
@@ -1,7 +1,8 @@
 #ifndef __AD_H__
 #define __AD_H__
 
+extern uint16_t AD_Value[4];
 void AD_Init(void);
-uint16_t AD_GetValue(uint8_t ADC_Channel);
+void AD_GetValue(void)
 
 #endif
